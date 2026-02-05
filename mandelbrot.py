e4ddd17742f2dc66663e55bfaff87b0ef079ad95
Ondros290 <zikan.ondrej@gmail.com>
2026-02-05 14:23:51 +0100

    Add mandelbrot skeleton

---
+++ mandelbrot.py
@@ -0,0 +1,20 @@
+"""
+Mandelbrot Set Generator
+Author : Ondrej Zikan
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+    """
+    Example function .
+    Parameters
+    ----------
+    x : float
+        Input value
+        
+    Returns
+    -------
+    float
+        Output value
+    """
+    # TODO : Implement the algorithm
+    pass
