30a65831f458fe5b66a85807ae36cf7dae9046d8
OnZik <ozikan25@student.aau.dk>
2026-02-19 09:10:33 +0100

    forgotten commit

--- mandelbrot.py
+++ mandelbrot.py
@@ -13,13 +13,14 @@ import time
 max_iter = 100
 
 def mandelbrod_point(c):
-    z = 0 + 0j
+    z = 0
     n_iter = 0
     for n in range(max_iter):
         n_iter = n
         z = z*z + c
         if (abs(z) > 2):
             break;
+            
     return n_iter
   
 def compute_mandelbrot(xmin, xmax, ymin, ymax, width, height):
@@ -35,12 +36,12 @@ def compute_mandelbrot(xmin, xmax, ymin, ymax, width, height):
             iter_count = mandelbrod_point(real + imag*1j)
            
             output[x][y] = iter_count
-      
+     
     return output
             
        
 start = time.time ()
-result = compute_mandelbrot( -2 , 1, -1.5 , 1.5 , 1024 , 1024)
+result = compute_mandelbrot( -2 , 1, -1.5 , 1.5 , 10 , 10)
 elapsed = time.time() - start
 print (f" Computation took { elapsed :.3f} seconds ")
 
