f80c633306c8ae6ad121cc8bc6fdbc10a845a2b5
OnZik <ozikan25@student.aau.dk>
2026-02-19 18:02:25 +0100

    week 2 - Milestone 4 - problem size scaling

--- mandelbrot_numpy.py
+++ mandelbrot_numpy.py
@@ -15,7 +15,8 @@ Course : Numerical Scientific Computing 2026
 
 import numpy as np
 import matplotlib.pyplot as plt
-from benchmark import benchmark
+#from benchmark import benchmark
+import timeit
 
 
   
@@ -46,14 +47,43 @@ def compute_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter):
     
     return M
 
-t , M = benchmark (compute_mandelbrot , -2, 1, -1.5 , 1.5 , 1024 , 1024 , 100)
-            
+def show_mandelbrot(n):
+    M = compute_mandelbrot(-2, 1, -1.5 , 1.5 , n , n , 100)
+    
+    plt.imshow(M, cmap='viridis')
+    plt.colorbar()
+    plt.title("Mandelbrot")
+    plt.show
+    # plt.savefig("mandelbrot.png")
+    
+
+# t , M = benchmark (compute_mandelbrot , -2, 1, -1.5 , 1.5 , 1024 , 1024 , 100)
+       
+def show_comp_time():
+    grid_sizes = [256, 512, 1024, 2048, 4096]
+    runtimes = []  
+    for n in grid_sizes:
+        t = timeit.timeit(lambda: compute_mandelbrot(-2, 1, -1.5 , 1.5 , n , n , 100), number=1)
+        runtimes.append(t)
+        print(f"Grid: {n}x{n}, Time: {t:.4f} seconds")
+    
+    # plot grid size vs. time
+    plt.figure()
+    plt.plot(grid_sizes, runtimes)
+    plt.xlabel("Grid Size")
+    plt.ylabel("Runtime")
+    plt.show()
+    
+show_comp_time()
+show_mandelbrot(1024)
+        
+        
 
 
-plt.imshow(M, cmap='viridis')
-plt.colorbar()
-plt.title("Mandelbrot")
-plt.show
-plt.savefig("mandelbrot.png")
+
+
+
+
+
 
 
