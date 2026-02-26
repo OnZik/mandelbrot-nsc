46f7d78b90a61a16e49a5804f80dca2d66b36bb2
OnZik <ozikan25@student.aau.dk>
2026-02-26 14:24:54 +0100

    added cProfiler

---
+++ cProfiling.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 26 13:55:38 2026
+
+@author: zikan
+"""
+
+import cProfile, pstats
+from mandelbrot_naive import mandelbrot_naive
+from mandelbrot_numpy import mandelbrot_numpy
+
+cProfile.run('mandelbrot_naive(-2,1,-1.5,1.5,512,512,100)',
+             'naive_profile.prof')
+cProfile.run('mandelbrot_numpy( -2,1,-1.5 ,1.5 ,512,512,100)',
+             'numpy_profile.prof')
+
+for name in ('naive_profile.prof', 'numpy_profile.prof'):
+    stats = pstats.Stats(name)
+    stats.sort_stats('cumulative')
+    stats.print_stats(10)
+    
+    
